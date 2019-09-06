7a1c91254ea5791df597b93e25b9fbf94f9fb609
akinegb <Akinegb@gmail.com>
2019-09-06 08:10:42 +0100

    add test to populate async data into table on page load

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { Table } from './App';
+import Enzyme, { mount } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import { getStarWarsCharacters } from "./services";
+import { act } from "react-dom/test-utils";
+Enzyme.configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  act(() => {
+    ReactDOM.render(<App />, div);
+  })
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('Load results into table', () => {
+  let wrapper = null;
+
+  beforeEach(() => {
+    return getStarWarsCharacters()
+      .then(res => {
+        act(() => {
+          wrapper = mount(<Table results={res.results}/>);
+        })
+      })
+  });
+
+  it("on page load", () => {
+    act(() => {
+      expect(wrapper.find(".search-result").length).toBeGreaterThan(1);
+    })
+  })
+})
